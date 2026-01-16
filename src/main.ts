4980e5ce3cbf6095b6d29b8869da5a9a0c94b12d
Miguel Ripoll <3296866+MiguelRipoll23@users.noreply.github.com>
2026-01-16 16:27:13 +0100

    Remove OpenTelemetry integration

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { Container } from "@needle-di/core";
 import { HTTPService } from "./core/services/http-service.ts";
 import { DatabaseService } from "./core/services/database-service.ts";
-import { OTelService } from "./api/versions/v1/services/otel-service.ts";
+
 
 const container = new Container();
 
-const otelService = container.get(OTelService);
-await otelService.init();
+
 
 const databaseService = container.get(DatabaseService);
 databaseService.init();
@@ -16,12 +15,10 @@ await httpService.listen();
 
 Deno.addSignalListener("SIGTERM", async () => {
   console.log("SIGTERM received. Shutting down gracefully...");
-  await otelService.shutdown();
   Deno.exit(0);
 });
 
 Deno.addSignalListener("SIGINT", async () => {
   console.log("SIGINT received. Shutting down gracefully...");
-  await otelService.shutdown();
   Deno.exit(0);
 });
