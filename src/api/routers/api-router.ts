69df6cdfdb97e734ef66c0bedbb64dc83e53bf14
Miguel Ripoll <3296866+MiguelRipoll23@users.noreply.github.com>
2026-02-07 18:32:09 +0100

    Call runStartupTasks() on v1 router directly from main.ts

--- src/api/routers/api-router.ts
+++ src/api/routers/api-router.ts
@@ -10,7 +10,6 @@ export class APIRouter {
     this.app = new OpenAPIHono();
     this.setMiddlewares();
     this.setRoutes();
-    this.runStartupTasks();
   }
 
   public getRouter(): OpenAPIHono {
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { Container } from "@needle-di/core";
 import { HTTPService } from "./core/services/http-service.ts";
 import { DatabaseService } from "./core/services/database-service.ts";
+import { V1Router } from "./api/versions/v1/routers/v1-rooter.ts";
 
 const container = new Container();
 
@@ -8,6 +9,10 @@ const databaseService = container.get(DatabaseService);
 databaseService.init();
 
 const httpService = container.get(HTTPService);
+
+const v1Router = container.get(V1Router);
+v1Router.runStartupTasks();
+
 await httpService.listen();
 
 Deno.addSignalListener("SIGTERM", async () => {
