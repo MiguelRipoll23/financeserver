aa6a0cd9014412659d5d68eaa10c074db3374179
Miguel Ripoll <3296866+MiguelRipoll23@users.noreply.github.com>
2026-03-06 14:08:36 +0100

    Add name column to bills

---
+++ drizzle/0007_first_tigra.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "bills" DROP CONSTRAINT "bills_email_id_bill_email_id_fk";
+--> statement-breakpoint
+ALTER TABLE "bills" ADD COLUMN "name" varchar(255) NOT NULL;--> statement-breakpoint
+ALTER TABLE "bills" DROP COLUMN "email_id";
